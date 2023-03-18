c4ff77dd7ad6eb660f3f72d06646428684d2f60a
i252-hub <74281449+i252-hub@users.noreply.github.com>
2023-03-18 08:56:45 +0800

    Create 1.js

---
+++ Javascript/1.js
@@ -0,0 +1,35 @@
+//Day 1
+
+//1. comments can make code readable
+//2. Welcome to 30DaysOfJavaScript
+/* 3. comments can make code readable, easy to reuse and informative*/
+
+/*4. mango = "This is a mango" | This is a string data type
+apple = true | This is a boolean data type
+let orange | This is a undefined data type
+banana = null| This is an empty data type which returns an empty value
+
+console.log(mango, apple,orange, banana) | print statement*/
+
+/*5. mango = "This is a mango" 
+apple = true 
+let orange 
+banana = null
+
+console.log(mango, ": ", typeof mango, "\n",apple, ": ", typeof apple, "\n",orange,": ", typeof orange,
+"\n", banana, ": ",typeof banana) \ Checking Datatypes*/ 
+
+/*6.
+var value
+var user
+let go
+let tuce |   Variables*/ 
+
+
+
+
+
+
+
+
+

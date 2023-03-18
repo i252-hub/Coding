6501908ba3b4bbb0628233aabae37e15cb3340a1
i252-hub <74281449+i252-hub@users.noreply.github.com>
2023-03-18 08:59:16 +0800

    Create 4.js

---
+++ Javascript/4.js
@@ -0,0 +1,171 @@
+/*age = parseInt(prompt("Enter your age: "))
+
+result = (2023-age)
+total = result-2005
+if(age>=18){
+    document.getElementById("name").innerHTML= 'You are old enough to drive.'
+}
+else{
+    document.getElementById("name").innerHTML= 'You are left with ' + total + ' years to drive.' 
+}*/
+
+/*age = parseInt(prompt("Enter your age: "))
+gap = age - 21
+gap_age = 21-age
+if(age>21){
+    document.getElementById("name").innerHTML= "You are " +gap + " years older than me."
+}
+else if(age === 21){
+    document.getElementById("name").innerHTML= "You are " + " the same age as me."
+}
+else{
+    document.getElementById("name").innerHTML= "I am " +gap_age + " years older than you."
+
+}*/
+
+/*let a = 4
+let b = 3
+
+if(a>b){
+    document.getElementById("name").innerHTML= a+ " is greater than " +b
+}
+else{
+    document.getElementById("name").innerHTML= b+ " is less than " +a
+}*/
+
+/*let number = parseInt(prompt("Enter a number: "))
+if(number%2==0){
+    document.getElementById("name").innerHTML= number + " is an even number."
+
+}
+else{
+    document.getElementById("name").innerHTML= number + " is an odd number."
+
+}*/
+
+/*
+90-100, A
+70-89, B
+60-69, C
+50-59, D
+0-49, F
+*/ 
+
+/*grade = parseInt(prompt("Enter Grade: "))
+
+if(grade <= 100 && grade >= 90){
+    document.getElementById("name").innerHTML= "Your grade is A";
+}
+else if(grade <= 80 && grade >= 70){
+    document.getElementById("name").innerHTML= "Your grade is B";
+}
+else if(grade <= 69 && grade >= 60){
+    document.getElementById("name").innerHTML= "Your grade is C";
+}
+else if(grade <= 59 && grade >= 50){
+    document.getElementById("name").innerHTML= "Your grade is D";
+}
+else if(grade <= 49 && grade >= 0){
+    document.getElementById("name").innerHTML= "Your grade is F";
+}
+else{
+    document.getElementById("name").innerHTML= "Invalid";
+
+}*/
+
+/*September, October or November, the season is Autumn.
+December, January or February, the season is Winter.
+March, April or May, the season is Spring
+June, July or August, the season is Summer */
+
+/*season = prompt('Enter month: ')
+if(season === 'March' || season === 'April' | season === 'May'){
+    document.getElementById("name").innerHTML= "The season is Spring";
+
+}
+else if(season === 'June' || season === 'July' | season === 'August'){
+    document.getElementById("name").innerHTML= "The season is Summer";
+
+}
+else if(season === 'September' || season === 'October' | season === 'November'){
+    document.getElementById("name").innerHTML= "The season is Autumn";
+
+}
+else if(season === 'December' || season === 'January' | season === 'February'){
+    document.getElementById("name").innerHTML= "The season is Winter";
+
+}
+
+else{
+    document.getElementById("name").innerHTML= "Enter a month!";
+}*/
+
+/*day = prompt("What is the day today? ")
+switch(day){
+    case 'Monday':
+        document.getElementById("name").innerHTML= day + " is a weekday.";
+        break;
+    case 'Tuesday':
+        document.getElementById("name").innerHTML= day + " is a weekday.";
+        break;
+    case 'Wednesday':
+        document.getElementById("name").innerHTML= day + " is a weekday.";
+        break;
+    case 'Thursday':
+        document.getElementById("name").innerHTML= day + " is a weekday.";
+        break;
+    case 'Friday':
+        document.getElementById("name").innerHTML= day + " is a weekday.";
+        break;
+    case 'Saturday':
+        document.getElementById("name").innerHTML= day + " is a weekend.";
+        break;
+    case 'Sunday':
+        document.getElementById("name").innerHTML= day + " is a weekend.";
+        break;
+   
+
+}*/
+
+/*let month = prompt("Enter the month: ")
+switch(month){
+    case "January":
+        document.getElementById("name").innerHTML= month + " has 31 days";
+        break;
+    case "February":
+        document.getElementById("name").innerHTML= month + " has 28 days";
+        break;
+    case "March":
+        document.getElementById("name").innerHTML= month + " has 31 days";
+        break;
+    case "April":
+        document.getElementById("name").innerHTML= month + " has 30 days";
+        break;
+    case "May":
+        document.getElementById("name").innerHTML= month + " has 31 days";
+        break;
+    case "June":
+        document.getElementById("name").innerHTML= month + " has 30 days";
+        break;
+    case "July":
+        document.getElementById("name").innerHTML= month + " has 31 days";
+        break;
+    case "August":
+        document.getElementById("name").innerHTML= month + " has 31 days";
+        break;
+    case "September":
+        document.getElementById("name").innerHTML= month + " has 30 days";
+        break;
+    case "October":
+        document.getElementById("name").innerHTML= month + " has 31 days";
+        break;
+    case "November":
+        document.getElementById("name").innerHTML= month + " has 30 days";
+        break;
+      case "December":
+        document.getElementById("name").innerHTML= month + " has 31 days";
+        break;
+}*/
+
+
+
